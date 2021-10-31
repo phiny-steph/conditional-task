07acbe257d3266e375845da0931caaaf1fabd13c
phiny-steph <86403160+phiny-steph@users.noreply.github.com>
2021-10-31 02:10:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -398,7 +398,37 @@ var shirtSleeve = 8.71;
  * [18, 29, 8.47, 'NA']
 */
 // WRITE YOUR CODE HERE
- 
+
+
+var shirtWidth = 28;
+var shirtLength = 34;
+var shirtSleeve = 10.13; 
+
+if ((shirtWidth == 18 && shirtWidth < 20) && (shirtLength ==28 && shirtLength < 29) && (shirtSleeve == 8.13 && shirtSleeve < 8.63)) {
+   console.log("S, size small.");
+
+} else if ((shirtWidth >= 20 && shirtWidth < 22) && (shirtLength >=29 && shirtLength < 30) && (shirtSleeve >= 8.38 && shirtSleeve < 8.63)){
+  console.log("M, size medium.");
+
+} else if ((shirtWidth >= 22 && shirtWidth < 24) && (shirtLength >=30 && shirtLength < 31) && (shirtSleeve >= 8.63 && shirtSleeve < 8.88)){
+  console.log("L, size large.");
+
+} else if ((shirtWidth >= 24 && shirtWidth < 26) && (shirtLength >=31 && shirtLength < 33) && (shirtSleeve >= 8.88 && shirtSleeve < 9.63)){
+  console.log("XL, size extra large.");
+
+} else if ((shirtWidth >= 26 && shirtWidth < 28) && (shirtLength >=33 && shirtLength < 34) && (shirtSleeve >= 9.63 && shirtSleeve < 10.13)){
+  console.log("2XL, size double extra large.");
+
+}else if ((shirtWidth >= 28) && (shirtLength >=34) && (shirtSleeve >= 10.13)){
+  console.log("3XL, size triple extra large.");
+}
+
+ else {
+  console.log("N/A");
+}
+
+
+
  
  
 
